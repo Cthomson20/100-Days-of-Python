2e6766e950026dfe9fdb356302c29ee92a29691b
Cthomson20 <christopherthomson2026@gmail.com>
2024-11-10 14:03:38 -0700

    updated file description

--- band_name_gen.py
+++ band_name_gen.py
@@ -1,4 +1,5 @@
 
+# Simple user input, text based project to practice string concatenation, variable, user input
 start = "Welcome to the Band Name Generator!"
 print(start)
 city = input("What's the name of the city you grew  up in?\n")
